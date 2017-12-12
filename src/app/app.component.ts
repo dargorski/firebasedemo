c8fd853809680f622c444fc7a994c2b813f14a70
Dariusz Gorski <dariusz.gorski@comarch.pl>
2017-12-12 17:23:20 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
+import { AngularFireDatabase } from 'angularfire2/database';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AngularFireList } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  courses$;
+  courseList: AngularFireList<any>;
+  course$;
+  author$;
+  subscription: Subscription;
+
+  constructor(db: AngularFireDatabase) {
+    this.courseList = db.list('/courses');
+    this.courses$ = this.courseList.valueChanges();
+    this.course$ = db.object('/courses/1').valueChanges();
+    this.author$ = db.object('/authors/1').valueChanges();
+  }
+
+  add(course: HTMLInputElement) {
+    this.courseList.push({
+      name: course.value,
+      price: 150,
+      isLive: true,
+      sections: [
+        {title: 'Components'},
+        {title: 'Directives'},
+        {title: 'Templates'}
+      ]
+    });
+    course.value = '';
+    console.log(this.courses$);
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyCd5m9s0N59JmJYURkHesnCVcwZuxkN0DE",
+  authDomain: "fir-demo-b0d93.firebaseapp.com",
+  databaseURL: "https://fir-demo-b0d93.firebaseio.com",
+  projectId: "fir-demo-b0d93",
+  storageBucket: "fir-demo-b0d93.appspot.com",
+  messagingSenderId: "128138677990"
+  }
 };
